cb9f3ec58a17e8b857e961fab7e62a7501887a5a
Nuelik <137001019+Nuelik@users.noreply.github.com>
2023-07-27 16:20:10 -0700

    onboarding updates

--- lib/Presentation/Screens/Onboarding_screen/add_location_screen.dart
+++ lib/Presentation/Screens/Onboarding_screen/add_location_screen.dart
@@ -8,21 +8,18 @@ class AddLocationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 18,
-              top: 30,
-            ),
-            child: Row(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
               children: [
                 GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     Navigator.pop(context);
-                    },
+                  },
                   child: const Icon(
                     Icons.arrow_back_ios,
                     color: Colors.black,
@@ -30,47 +27,61 @@ class AddLocationScreen extends StatelessWidget {
                 ),
               ],
             ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          const Center(
-              child: Text(
-            "Where do you live ?",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          )),
-         Center(child: Image.asset("assets/images/location2.png")),
-         const SizedBox(height: 60,),
-         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-          Icon(Icons.location_on_outlined),
-          Text("Select your current location:",style: TextStyle(fontSize: 20),),
-          Text("Cameroon",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 20),)
-         ],),
-          const SizedBox(height: 30,),
-          Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: ClipRRect(
+            const SizedBox(
+              height: 40,
+            ),
+            const Center(
+                child: Text(
+              "Where do you live ?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            )),
+            Center(child: Image.asset("assets/images/location2.png")),
+            const SizedBox(
+              height: 40,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.location_on_outlined),
+                Text(
+                  "Select your current location:",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "Cameroon",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: ClipRRect(
                 borderRadius: BorderRadius.circular(20),
                 child: MaterialButton(
-                  height: 50,
-                  minWidth: double.infinity,
-                  color: Colors.black,
-                  child: const Text("Next",style: TextStyle(color: Colors.white,fontSize: 20),),
-                  onPressed: (){
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: ((context) =>  AddReligionScreen())));
-                  }),
+                    height: 50,
+                    minWidth: double.infinity,
+                    color: Colors.black,
+                    child: const Text(
+                      "Next",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: ((context) => AddReligionScreen())));
+                    }),
               ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-        ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
