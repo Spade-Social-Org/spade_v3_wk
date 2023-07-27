60a4232b3e43c7992dc9080b2e27c67f9ac5b52d
Nuelik <137001019+Nuelik@users.noreply.github.com>
2023-07-27 16:58:03 -0700

    onboarding updates

--- lib/Presentation/Screens/Onboarding_screen/add_location_screen.dart
+++ lib/Presentation/Screens/Onboarding_screen/add_location_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spade_v4/Common/extensions/barrel_extensions.dart';
 
 import 'add_religion_screen.dart';
 
@@ -27,8 +28,8 @@ class _AddLocationScreenState extends State<AddLocationScreen> {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
           children: [
             Row(
               children: [
@@ -55,9 +56,11 @@ class _AddLocationScreenState extends State<AddLocationScreen> {
             const SizedBox(
               height: 40,
             ),
+            //Country and State Row
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                //country
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -65,31 +68,139 @@ class _AddLocationScreenState extends State<AddLocationScreen> {
                       "Country",
                       style: TextStyle(fontSize: 16),
                     ),
-                    DropdownButtonFormField(
-                      value: selectedValue,
-                      items: dropdownItems,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          selectedValue = newValue!;
-                        });
-                      },
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(width: 2),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(width: 2),
-                          borderRadius: BorderRadius.circular(20),
+                    Expanded(
+                      child: DropdownButtonFormField(
+                        value: selectedValue,
+                        items: dropdownItems,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedValue = newValue!;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
                         ),
                       ),
                     ),
                   ],
-                )
+                ), // end of country
+                SizedBox(
+                  width: 18.width(),
+                ),
+                //State
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "State",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Expanded(
+                      child: DropdownButtonFormField(
+                        value: selectedValue,
+                        items: dropdownItems,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedValue = newValue!;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ), // end of State
               ],
+            ), // end of country and state Row
+            SizedBox(
+              height: 20.height(),
             ),
+            //city and postal_code Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //city
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "City",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Expanded(
+                      child: DropdownButtonFormField(
+                        value: selectedValue,
+                        items: dropdownItems,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedValue = newValue!;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ), // end of city
+                const SizedBox(
+                  width: 18,
+                ),
+                //postal_code
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Postal Code",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "1111",
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ), // end of postal_code
+              ],
+            ), // end of city and postal_code Row
             const SizedBox(
-              height: 20,
+              height: 30,
             ),
             Padding(
               padding: const EdgeInsets.all(18.0),
