09e5150ac4643fe1775d69e44d4d7ee39dadeb1d
Anthonycodes101 <anthonyuzonna341@gmail.com>
2023-07-28 16:40:26 -0700

    auth structure

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_tinder_swipe
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTinderSwipePlugin.register(with: registry.registrar(forPlugin: "FlutterTinderSwipePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
