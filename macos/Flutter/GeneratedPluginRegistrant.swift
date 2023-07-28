b4af8d57bd48a393f8cb329bd1ce510474b868a8
Nuelik <137001019+Nuelik@users.noreply.github.com>
2023-07-28 04:50:22 -0700

    onboarding updates

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_tinder_swipe
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterTinderSwipePlugin.register(with: registry.registrar(forPlugin: "FlutterTinderSwipePlugin"))
 }
