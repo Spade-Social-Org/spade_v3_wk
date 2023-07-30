ac4b6aafdffab0b290f86fc1e371f8220900c884
Mmegwa Anthony <106021539+anthonycodes1@users.noreply.github.com>
2023-07-30 10:09:17 +0100

    Revert "changed file sizing"

--- lib/Presentation/Screens/Friends/friends_screen.dart
+++ lib/Presentation/Screens/Friends/friends_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:spade_v4/Common/extensions/barrel_extensions.dart';
-
 class FriendsScreen extends StatefulWidget {
   const FriendsScreen({Key? key}) : super(key: key);
 
@@ -11,14 +9,6 @@ class FriendsScreen extends StatefulWidget {
 class _FriendsScreenState extends State<FriendsScreen> {
   @override
   Widget build(BuildContext context) {
-    // ignore: sized_box_for_whitespace
-    return Padding(
-      padding:  EdgeInsets.only(left: 25.width(), bottom: 20.height()),
-      child: Container(
-        height: 30.height(),
-        width: 200.width(),
-      ),
-    );
-    
+    return const Placeholder();
   }
 }
