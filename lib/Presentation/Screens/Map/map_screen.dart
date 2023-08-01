6d46d8b2a245237c4287609a87237715f637a3f9
Anthonycodes101 <anthonyuzonna341@gmail.com>
2023-08-01 13:07:01 -0700

    Map/done

--- lib/Presentation/Screens/Map/map_screen.dart
+++ lib/Presentation/Screens/Map/map_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -7,6 +6,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../widgets/jh_custom_marker.dart';
+import '../../widgets/jh_loader.dart';
 import '../../widgets/jh_map_style.dart';
 import '../Global/global.dart';
 
@@ -26,7 +26,10 @@ class _GoogleMapState extends State<GoogleMapScreen>
   TextEditingController _searchController = TextEditingController();
 
   Map<String, Marker> _markers = {};
+  Set<Polyline> polylines = {};
   bool loadingLocation = true;
+  bool isLocationEnabled = false;
+  bool trafficEnabled = false;
   late Geolocator geolocator;
   LatLng? _initialPosition;
   LatLng? _searchedLocation;
@@ -61,6 +64,19 @@ class _GoogleMapState extends State<GoogleMapScreen>
       });
     }
     _getCurrentLocation();
+  }
+
+
+  void _toggleLocation() {
+    setState(() {
+      isLocationEnabled = !isLocationEnabled;
+    });
+
+    if (isLocationEnabled) {
+      _getCurrentLocation();
+    } else {
+      _initialPosition = null;
+    }
   }
 
   void _getCurrentLocation() async {
@@ -110,12 +126,24 @@ class _GoogleMapState extends State<GoogleMapScreen>
         _initialPosition = LatLng(position.latitude, position.longitude);
         _searchController.text = userAddress;
         loadingLocation = false;
+        //isLocationEnabled = true;
       });
+      _addCircle(position);
       double markerOffset = 0.0002;
       addMarker(
         'USER',
         LatLng(position.latitude + markerOffset, position.longitude),
       );
+      Polyline polyline = Polyline(
+        polylineId: PolylineId('polyline_1'),
+        color: Colors.blue,
+        width: 5,
+        points: [LatLng(position.latitude, position.longitude)],
+      );
+
+      setState(() {
+        polylines = {polyline};
+      });
     } on PlatformException catch (e) {
       /// Handle errors that might occur when fetching the current location
       print("Error: ${e.message}");
@@ -156,11 +184,23 @@ class _GoogleMapState extends State<GoogleMapScreen>
       body: Stack(
         children: [
           GoogleMap(
+            rotateGesturesEnabled: true,
+            myLocationEnabled: isLocationEnabled,
+            myLocationButtonEnabled: false,
+            fortyFiveDegreeImageryEnabled: true,
+            trafficEnabled: trafficEnabled,
             circles: _circle,
+            polylines: polylines,
             onMapCreated: (controller) {
               controller.setMapStyle(mapTheme);
               mapController = controller;
+
+              /// After fetching the location this [SETSTATE] we set the isLocationEnabled to true meaning the button is on
+              setState(() {
+                isLocationEnabled = true;
+              });
             },
+
             markers: _markers.values.toSet(),
             initialCameraPosition: _initialPosition != null
                 ? CameraPosition(target: _initialPosition!, zoom: 14)
@@ -168,7 +208,7 @@ class _GoogleMapState extends State<GoogleMapScreen>
           ),
           if (loadingLocation)
             const Center(
-              child: CircularProgressIndicator(),
+              child: LoadingSpinner(),
             ),
           Positioned(
             left: 20,
@@ -217,12 +257,23 @@ class _GoogleMapState extends State<GoogleMapScreen>
                 Expanded(
                   child: TextField(
                     controller: _searchController,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
                     decoration: InputDecoration(
-                      hintText: "Search location",
+                      hintText: "Where would you like to go?",
+                      hintStyle: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                       fillColor: Colors.grey.withOpacity(0.8),
                       contentPadding: EdgeInsets.symmetric(vertical: 12),
                       filled: true,
                       border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
                         borderSide: const BorderSide(
                           color: Colors.white,
@@ -230,10 +281,16 @@ class _GoogleMapState extends State<GoogleMapScreen>
                         ),
                       ),
                       suffixIcon: IconButton(
-                        icon: Icon(Icons.search),
+                        icon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          weight: 30,
+                          size: 30,
+                        ),
                         onPressed: _searchLocation,
                       ),
                     ),
+                    textAlign: TextAlign.center,
                     onSubmitted: (_) {
                       _searchLocation();
                     },
@@ -242,6 +299,43 @@ class _GoogleMapState extends State<GoogleMapScreen>
               ],
             ),
           ),
+          Positioned(
+            top: 36,
+            right: 20,
+            child: InkWell(
+              onTap: _toggleLocation,
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: isLocationEnabled ? Colors.green : Colors.grey,
+                    width: 5.0,
+                  ),
+                ),
+                child: CircleAvatar(
+                  backgroundColor: Colors.black,
+                  radius: 20,
+                  child: Image.asset('assets/images/Union.png'),
+                ),
+              ),
+            ),
+          ),
+          // Positioned(
+          //   top: 36,
+          //   left: 16,
+          //   child: Switch(
+          //     value: trafficEnabled,
+          //     onChanged: (value) {
+          //       setState(() {
+          //         trafficEnabled = value;
+          //       });
+          //     },
+          //     activeColor: Colors.green,
+          //     activeTrackColor: Colors.lightGreen,
+          //     inactiveThumbColor: Colors.grey,
+          //     inactiveTrackColor: Colors.grey.withOpacity(0.5),
+          //   ),
+          // ),
         ],
       ),
     );
@@ -249,17 +343,16 @@ class _GoogleMapState extends State<GoogleMapScreen>
 
   Set<Circle> _circle = {};
 
-  void _addCircle() {
-    if (_initialPosition != null) {
+  void _addCircle(Position position) {
+    if (position != null) {
       _circle.add(
         Circle(
           circleId: CircleId('circle_1'),
-          center:
-              LatLng(_initialPosition!.latitude, _initialPosition!.longitude),
+          center: LatLng(position.latitude, position.longitude),
           radius: 900,
           fillColor: Colors.grey.withOpacity(0.5),
-          strokeWidth: 4,
-          strokeColor: Colors.grey,
+          strokeWidth: 2,
+          strokeColor: Colors.green,
         ),
       );
       setState(() {});
@@ -279,7 +372,6 @@ class _GoogleMapState extends State<GoogleMapScreen>
       icon: await customMarkerIcon.createMarkerIcon(),
     );
     _markers[id] = marker;
-    _addCircle();
     setState(() {});
   }
 }
