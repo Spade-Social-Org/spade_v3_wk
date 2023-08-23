9b3e727fac59b159e260456cae08a430ad632cb8
Anthonycodes101 <anthonyuzonna341@gmail.com>
2023-08-23 13:22:21 +0100

    bottomsheet

---
+++ lib/Presentation/widgets/places_items.dart
@@ -0,0 +1,13 @@
+List<dynamic> images = [
+  "assets/images/Rectangle 3382.png",
+  "assets/images/Rectangle 3381.png",
+  "assets/images/Rectangle 3383.png",
+  "assets/images/Rectangle 3384.png"
+];
+
+List<String> title = [
+  "Restaurant",
+  "Movies",
+  "Activities",
+  "Hotel",
+];
