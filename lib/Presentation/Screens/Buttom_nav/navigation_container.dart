752b3d17738ce5debce341b29d2aed9bc269a8ec
supreme2580 <victoromorogbe69@gmail.com>
2023-11-17 01:56:12 +0100

    lite

--- lib/Presentation/Screens/Buttom_nav/navigation_container.dart
+++ lib/Presentation/Screens/Buttom_nav/navigation_container.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spade_v4/Data/Models/discover_service.dart';
-import 'package:spade_v4/Presentation/Screens/Discover/discover_screen.dart';
 import 'package:spade_v4/Presentation/Screens/Home/providers/feed_provider.dart';
 
 import '../Camera/camera_screen.dart';
 import '../Home/presentation/home_screen.dart';
 import '../Map/map_screen.dart';
-import '../More_screen/more_screen.dart';
 import '../messages/message_screen.dart';
 
 class NavigationContainer extends StatefulWidget {
@@ -19,10 +17,10 @@ class NavigationContainer extends StatefulWidget {
 }
 
 class _NavigationContainerState extends State<NavigationContainer> {
-  int selectedIndex = 2;
-  int _PageIndex = 2;
+  int selectedIndex = 0;
+  int _PageIndex = 0;
   bool _showOption = false;
-  int card_click = 2;
+  int card_click = 0;
   PageController get _pageController => FeedRepo.pageController;
 
   @override
@@ -30,9 +28,7 @@ class _NavigationContainerState extends State<NavigationContainer> {
     final List<Widget> pages = [
       const HomeScreen(),
       const MessageScreen(),
-      const DiscoveryScreen(),
       const GoogleMapScreen(),
-      const MoreScreen(),
     ];
 
     final List<Widget> appPage = [
@@ -108,20 +104,6 @@ class _NavigationContainerState extends State<NavigationContainer> {
                   ),
                   BottomNavigationBarItem(
                     icon: Image.asset(
-                      _showOption
-                          ? selectedIndex == 2
-                              ? "assets/images/card-light.png"
-                              : "assets/images/bottom_nav.png"
-                          : selectedIndex == 2
-                              ? "assets/images/card-light.png"
-                              : "assets/images/flip-card.png",
-                      width: 24,
-                      height: 24,
-                    ),
-                    label: "",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset(
                       selectedIndex == 3
                           ? "assets/images/global-light.png"
                           : "assets/images/global.png",
@@ -130,121 +112,8 @@ class _NavigationContainerState extends State<NavigationContainer> {
                     ),
                     label: "",
                   ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset("assets/images/list-colored.png",
-                        width: 24, height: 24),
-                    label: "",
-                  ),
                 ],
               ),
-              if (_showOption)
-                Positioned(
-                  bottom: 1.0,
-                  child: Container(
-                    height: 75,
-                    width: 100,
-                    decoration: const BoxDecoration(
-                      // color: Colors.grey,
-                      image: DecorationImage(
-                          image: AssetImage("assets/images/bottom_nav.png")),
-                    ),
-                    padding: const EdgeInsets.all(8.0),
-                    child: Stack(
-                      fit: StackFit.expand,
-                      children: [
-                        Positioned(
-                            top: card_click == 1 ? 1 : 12,
-                            left: 0,
-                            child: GestureDetector(
-                              onTap: (() =>
-                                  {discover.updateIndex('red'), _zoneClick(1)}),
-                              child: Transform.rotate(
-                                angle: -0.5,
-                                child: Container(
-                                  height: card_click == 1 ? 35.0 : 20.0,
-                                  width: 15.0,
-                                  color: Colors.red,
-                                ),
-                              ),
-                            )),
-                        Positioned(
-                            left: 22.0,
-                            top: card_click == 2 ? -5.0 : 5.0,
-                            child: GestureDetector(
-                              onTap: (() => {
-                                    discover.updateIndex('green'),
-                                    _zoneClick(2)
-                                  }),
-                              child: Transform.rotate(
-                                angle: -0.2,
-                                child: Container(
-                                  height: card_click == 2 ? 35.0 : 20.0,
-                                  width: 15.0,
-                                  color: Colors.green.shade900,
-                                ),
-                              ),
-                            )),
-                        Positioned(
-                            left: 45.0,
-                            top: card_click == 3 ? -5.0 : 5.0,
-                            child: GestureDetector(
-                              onTap: (() => {
-                                    discover.updateIndex('yellow'),
-                                    _zoneClick(3)
-                                  }),
-                              child: Transform.rotate(
-                                angle: 0.2,
-                                child: Container(
-                                  height: card_click == 3 ? 35.0 : 20.0,
-                                  width: 15.0,
-                                  color: Colors.yellow,
-                                ),
-                              ),
-                            )),
-                        Positioned(
-                            left: 67.0,
-                            top: card_click == 4 ? 0.0 : 12.0,
-                            child: GestureDetector(
-                              onTap: (() => {
-                                    discover.updateIndex('grey'),
-                                    _zoneClick(4)
-                                  }),
-                              child: Transform.rotate(
-                                angle: 0.5,
-                                child: Container(
-                                  height: card_click == 4 ? 35.0 : 20.0,
-                                  width: 15.0,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            )),
-                        Positioned(
-                            left: 35.0,
-                            top: 40.0,
-                            child: GestureDetector(
-                              onTap: (() => {
-                                    if (_showOption)
-                                      {
-                                        setState(
-                                            () => _showOption = !_showOption)
-                                      }
-                                  }),
-                              child: Transform.rotate(
-                                angle: 0.0,
-                                child: Container(
-                                  height: 15.0,
-                                  width: 15.0,
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ),
-                            )),
-                      ],
-                    ),
-                  ),
-                ),
             ],
           ),
         );
@@ -263,36 +132,10 @@ class _NavigationContainerState extends State<NavigationContainer> {
     super.initState();
   }
 
-  void _onIconTapped(int index) {
-    setState(() {
-      selectedIndex = index;
-      _PageIndex = index;
-      _pageController.animateToPage(
-        index,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    });
-  }
-
   void _onPageChanged(int index) {
     if (_PageIndex != index) {
       setState(() {
         _PageIndex = index;
-      });
-    }
-  }
-
-  void _zoneClick(int index) {
-    if (card_click == index) {
-      setState(() {
-        card_click = 0;
-        _showOption = false;
-      });
-    } else {
-      setState(() {
-        card_click = index;
-        _showOption = false;
       });
     }
   }
--- lib/Presentation/Screens/Discover/widgets/spacer.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-heightSpace(double height) => SizedBox(height: height);
-widthSpace(double width) => SizedBox(width: width);
